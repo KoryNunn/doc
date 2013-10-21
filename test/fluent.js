affc78e7ede46cb5ccf39d06a8dbd83b5ceae5b6
Kory Nunn <knunn187@gmail.com>
2013-10-22 00:07:46 +1000

    added legacy tests

---
+++ test/fluent.js
@@ -0,0 +1,388 @@
+var doc = require('../'),
+    crel = require('crel');
+
+module.exports = function(test){
+
+    test('find spans', function(t) {
+        crel(document.body,
+            crel('span'),
+            crel('span'),
+            crel('span')
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('span')().length,
+            3,
+            'found 3 spans'
+        );
+        t.end();
+    });
+
+    test('find nested spans', function(t) {
+        crel(document.body,
+            crel('span',
+                crel('span',
+                    crel('span')
+                )
+            )
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('span')().length,
+            3,
+            'found 3 spans'
+        );
+        t.end();
+    });
+
+    test('find nested spans after context', function(t) {
+        crel(document.body,
+            crel('span',
+                crel('span',
+                    crel('span')
+                )
+            )
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('span').find('span')().length,
+            2,
+            'found 2 nested spans'
+        );
+        t.end();
+    });
+
+
+    test('find by class', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('.things')().length,
+            1,
+            'found 1 .things'
+        );
+        t.end();
+    });
+
+    test('find by class with reference', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('body').find('.things')().length,
+            1,
+            'found 1 .things'
+        );
+        t.end();
+    });
+
+    test('find with node reference', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc(document.body).find('.things')().length,
+            1,
+            'found 1 .things'
+        );
+        t.end();
+    });
+
+    test('is', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('.things').is('.things')(),
+            true,
+            '.things is .things'
+        );
+        t.end();
+    });
+
+    test('isnt..', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        t.equal(
+            doc('.things').is('.notThis')(),
+            false,
+            '.things is not .notThis'
+        );
+        t.end();
+    });
+
+    test('closest', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'},
+                crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(1);
+
+        var result = doc('.stuff').closest('.things')();
+
+        t.equal(
+            result.className,
+            'things',
+            'found closest stuff to things'
+        );
+        t.end();
+    });
+
+    test('closest via node', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'},
+                crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(1);
+
+        var result = doc(document.querySelector('.stuff')).closest('.things')();
+
+        t.equal(
+            result.className,
+            'things',
+            'found closest stuff to things'
+        );
+        t.end();
+    });
+
+    test('closest document', function(t) {
+        crel(document.body,
+            crel('div', {'class':'things'},
+                crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(1);
+
+        var result = doc(document).closest(document)();
+
+        t.equal(
+            result,
+            document,
+            'found closest document'
+        );
+        t.end();
+    });
+
+    test('on click', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things'},
+                crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(1);
+
+        doc('.things').on('click', function(){
+            t.pass('recieved click event');
+        });
+
+        targetElement.click();
+
+        t.end();
+    });
+
+    test('on click null target', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things'},
+                crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(1);
+
+        doc('nothing').on('click', function(){
+            t.fail('recieved click event');
+        });
+
+        targetElement.click();
+
+        t.pass('no click heard');
+
+        t.end();
+    });
+
+    test('off click', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things'},
+                crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(2);
+
+        var callback = function(){
+            t.pass('recieved click event');
+        };
+
+        doc('.things').on('click', callback);
+
+        targetElement.click();
+
+        doc('.things').off(callback);
+
+        t.pass('no click heard');
+
+        t.end();
+    });
+
+    test('on click delegated', function(t) {
+        var delegateElement,
+            targetElement;
+
+        crel(document.body,
+            delegateElement = crel('div', {'class':'things'},
+                targetElement = crel('div', {'class':'stuff'})
+            )
+        )
+
+        t.plan(1);
+
+        doc('.things').on('click', '.stuff', function(event){
+            if(event.target === targetElement){
+                t.pass('recieved click event');
+            }else{
+                t.fail('wrong target');
+            }
+        });
+
+        targetElement.click();
+
+        delegateElement.click();
+
+        t.end();
+    });
+
+    test('add class', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        doc('.things').addClass('stuff')();
+
+        t.equal(
+            targetElement.className,
+            'things stuff',
+            'target had new class'
+        );
+
+        t.end();
+    });
+
+    test('add classes', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        doc('.things').addClass('stuff majigger')();
+
+        t.equal(
+            targetElement.className,
+            'things stuff majigger',
+            'target had new classes'
+        );
+
+        t.end();
+    });
+
+    test('add dupliacte class', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things'})
+        )
+
+        t.plan(1);
+
+        doc('.things').addClass('stuff things')();
+
+        t.equal(
+            targetElement.className,
+            'things stuff',
+            'target had only new classes added'
+        );
+
+        t.end();
+    });
+
+    test('remove class', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things stuff'})
+        )
+
+        t.plan(1);
+
+        doc('.things').removeClass('things')();
+
+        t.equal(
+            targetElement.className,
+            'stuff',
+            'target had class removed'
+        );
+
+        t.end();
+    });
+
+    test('remove classes', function(t) {
+        var targetElement;
+
+        crel(document.body,
+            targetElement = crel('div', {'class':'things stuff'})
+        )
+
+        t.plan(1);
+
+        doc('.things').removeClass('things stuff')();
+
+        t.equal(
+            targetElement.className,
+            '',
+            'target had classes removed'
+        );
+
+        t.end();
+    });
+};
