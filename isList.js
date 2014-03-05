f9a28fa5aeb003447258e0a635efc944eab3c6e9
Maurice Butler <maurice.butler@gmail.com>
2014-03-05 14:01:44 +1000

    Improved list detection

--- isList.js
+++ isList.js
@@ -1,3 +1,8 @@
 module.exports = function isList(object){
-    return object !== window && (object instanceof Array || object && typeof object === 'object' && 'length' in object);
+    return object !== window && (
+        object instanceof Array ||
+        (typeof HTMLCollection !== 'undefined' && object instanceof HTMLCollection) ||
+        (typeof NodeList !== 'undefined' && object instanceof NodeList) ||
+        Array.isArray(object)
+    );
 }
