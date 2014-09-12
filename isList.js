5114e85e68ec6749f2038487e982c93a97c65a13
KoryNunn <knunn187@gmail.com>
2014-09-12 16:30:59 +1000

    IE check fixes

--- isList.js
+++ isList.js
@@ -1,8 +1,10 @@
 module.exports = function isList(object){
     return object !== window && (
         object instanceof Array ||
-        (typeof HTMLCollection !== 'undefined' && object instanceof HTMLCollection) ||
-        (typeof NodeList !== 'undefined' && object instanceof NodeList) ||
-        Array.isArray(object)
+        (typeof HTMLCollection === 'function' && object instanceof HTMLCollection) ||
+        (typeof NodeList === 'function' && object instanceof NodeList) ||
+        Array.isArray(object) ||
+        ''+object === '[object StaticNodeList]' ||
+        ''+object === '[object HTMLCollection]'
     );
 }
