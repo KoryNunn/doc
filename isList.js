7fa201dff5ede08383de3aae46886aeea49fe5d5
KoryNunn <knunn187@gmail.com>
2014-11-26 14:20:14 +1000

    Fixed IE8 idiocy

--- isList.js
+++ isList.js
@@ -1,3 +1,3 @@
 module.exports = function isList(object){
-    return object != null && typeof object === 'object' && 'length' in object && !('nodeType' in object) && object.self !== object;
+    return object != null && typeof object === 'object' && 'length' in object && !('nodeType' in object) && object.self != object; // in IE8, window.self is window, but it is not === window, but it is == window......... WTF!?
 }
